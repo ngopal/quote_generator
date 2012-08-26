643eb358acd9594bceed876df0f6bc86774d37f0
Nikhil Gopal <nikhilgopal@gmail.com>
2012-08-26 01:16:23 -0700

    quote by id route

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ app = Flask(__name__)
 def connect_db():
 	return sqlite3.connect(DATABASE)
 
+def get_connection():
+	db  = getattr(g, '_db', None)
+	if db is None:
+		db = g._db = connect_db()
+	return db
+
 def query_db(query, args=(), one=False):
 	cur = g.db.execute(query, args)
 	rv = [dict((cur.description[idx][0], value) for idx, value in enumerate(row)) for row in cur.fetchall()]
@@ -15,8 +21,22 @@ def query_db(query, args=(), one=False):
 
 @app.route('/')
 def index():
-	quote = 'Random Quote Goes Here'
+	def generate_quote():
+		index = str(random.randint(0,640))
+		randquery = 'SELECT * FROM quotes'
+		for k in query_db(randquery):
+			print k
+		return randquery 
+	try:
+		quote = generate_quote()
+	except:
+		quote = 'Random Quote Goes Here'
 	return render_template('main.html', quote=quote)
 
+@app.route('/quote/<id>')
+def show_quote_by_id(id):
+	return 'Quote by ID %s' % id
+
 if __name__ == "__main__":
+	app.debug = True
 	app.run()
